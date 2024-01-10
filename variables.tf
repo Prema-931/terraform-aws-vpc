84d87b270782447a56b794c7f169c5bec94584e5
Prema-931 <premaskht@gmail.com>
2024-01-10 13:44:58 +0530

    terraform-aws-vpc

---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"   # users can override
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true
+}
+
+variable "common_tags" {
+    type = map
+    default = {}  #it is optional
+
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}  
+
+}
+
+variable "project_name" {
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "igw_tags" {
+    type = map  
+    default = {}
+  
+}
+
+variable "public_subnet_cidr" {
+    type = list 
+    validation {
+      condition = length(var.public_subnet_cidr) ==2
+      error_message = "Please give2 public valid subnets CIDR"
+
+    }
+  
+}
+
+variable "public_subnet_tags" {
+    default = {}
+  
+}
+
+variable "private_subnet_cidr" {
+    type = list 
+    validation {
+      condition = length(var.private_subnet_cidr) ==2
+      error_message = "Please give2 private valid subnets CIDR"
+
+    }
+  
+}
+
+variable "private_subnet_tags" {
+    default = {}
+  
+}
