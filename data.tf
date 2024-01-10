023d0ef9400e6a58106e3c6f35db88f5c291aa61
Prema-931 <premaskht@gmail.com>
2024-01-10 17:32:55 +0530

    terraform-aws-vpc

--- data.tf
+++ data.tf
@@ -1,3 +1,18 @@
 data "aws_availability_zones" "azs" {
     state = "available"
 }
+
+data "aws_vpc" "default" {
+   # type = bool
+    default = true
+}
+
+data "aws_route_table" "default" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+      name = "association.main"
+      values = ["true"]
+
+
+    }
+}
--- variables.tf
+++ variables.tf
@@ -65,3 +65,48 @@ variable "private_subnet_tags" {
     default = {}
   
 }
+
+variable "database_subnet_cidr" {
+    type = list 
+    validation {
+      condition = length(var.database_subnet_cidr) ==2
+      error_message = "Please give2 database valid subnets CIDR"
+
+    }
+  
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+variable "is_peering_required" {
+    type = bool
+    default = false
+  
+}
+
+variable "acceptor_vpc_id" {
+    type = string
+    default = ""
+  
+}
+
+variable "vpc_peering_tags" {
+  default = {}
+}
